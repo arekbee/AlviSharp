bc995aa062651b8408e1bbf4c677ce73c16d7852
Arkadiusz Beer <arekbee@pc9.home>
2016-02-03 00:06:28 +0100

    Fixing tests and select-alt clause

---
+++ Src/AlviSharp/Run/AlviSharp.Lts.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlviSharp.Lts.Cmd
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Src/AlviSharp/Src/AlviSharp.Lts/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlviSharp.Lts
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
